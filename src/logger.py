9349b849a1d5377db9a50b0505ff7e50f0ef7776
Manoj G <manojmanu914822@gmail.com>
2026-04-04 17:17:54 +0530

    Add Students Performance dataset

---
+++ src/logger.py
@@ -0,0 +1,23 @@
+import logging
+import os
+from datetime import datetime
+
+# 1️⃣ Log file name with timestamp
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+# 2️⃣ Logs folder
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+# 3️⃣ Full log file path
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# 4️⃣ Configure logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+# 5️⃣ Create logger object
+logger = logging.getLogger(__name__)
