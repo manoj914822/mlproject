e2756179791d03d7bd49ad6770df7f86af9c9075
Manoj G <manojmanu914822@gmail.com>
2026-04-05 11:49:49 +0530

    EDA and Problems statement

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''This function returns the list of requirements.'''
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements if req.strip() and not req.strip().startswith('#')]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='manoj',
+    author_email='manojmanu914822@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+)
+
+
+
+      
